f8ec76d99cae05fdbd812397e0da7d95be6ba824
Shane Drury <shane.r.drury@gmail.com>
2015-05-21 18:01:36 +0100

    Cleanup Game

--- roguelike/core/graphics.py
+++ roguelike/core/graphics.py
@@ -1,23 +1,30 @@
-from roguelike import libtcod
+import libtcod
 
 
 class Graphics(object):
     def __init__(self):
-        self.tcod = libtcod
+        self.t = libtcod
 
     def put_char(self, x, y, char):
-        self.tcod.console_put_char(0, x, y, char, libtcod.BKGND_NONE)
+        self.t.console_put_char(0, x, y, char, self.t.BKGND_NONE)
 
     def flush(self):
-        self.tcod.console_flush()
+        self.t.console_flush()
 
     def init_root(self, w, h, title, fullscreen=False, renderer=None):
-        self.tcod.console_init_root(w, h, title, fullscreen,
-                                    renderer=renderer or self.tcod.RENDERER_SDL)
+        self.t.console_init_root(w, h, title, fullscreen,
+                                 renderer=renderer or self.t.RENDERER_SDL)
 
     def set_custom_font(self, font_file, flags=None, nb_char_horiz=0, nb_char_vertic=0):
-        self.tcod.console_set_custom_font(
+        self.t.console_set_custom_font(
             font_file,
-            flags=flags or self.tcod.FONT_LAYOUT_ASCII_INCOL, nb_char_horiz=0,
-            nb_char_vertic=0
+            flags=flags or self.t.FONT_LAYOUT_ASCII_INCOL,
+            nb_char_horiz=nb_char_horiz,
+            nb_char_vertic=nb_char_vertic
         )
+
+    def is_window_closed(self):
+        return self.t.console_is_window_closed()
+
+    def set_default_foreground(self, console, colour):
+        self.t.console_set_default_foreground(console, colour)
