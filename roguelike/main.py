af96a7ceee21d4a97b540f2b2f4c51852139bfa1
Shane Drury <shane.r.drury@gmail.com>
2015-05-21 18:01:57 +0100

    Refactor main

--- roguelike/main.py
+++ roguelike/main.py
@@ -1,28 +1,23 @@
-import os
-
 import yaml
 
-from roguelike import libtcod
-from roguelike import settings
-from roguelike.core.graphics import Graphics
-from roguelike.player import Player
+from core.colour import Colour
+from core.font import Font
+from core.graphics import Graphics
+from core.keys import Keys
+from game import Game
+from player import Player
+import settings
 
-with open(settings.VARS_FILE, 'r') as f:
-    consts = yaml.load(f)
+def main():
+    with open(settings.VARS_FILE, 'r') as f:
+        consts = yaml.load(f)
+    graphics = Graphics()
+    keys = Keys()
+    player = Player(consts)
+    font = Font()
+    colour = Colour()
+    game = Game(graphics, keys, player, font, colour, settings)
+    game.main()
 
-graphics = Graphics()
-graphics.set_custom_font(
-    os.path.join(settings.FONT_DIR, 'arial10x10.png'),
-    libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD
-)
-graphics.init_root(title='Roguelike', fullscreen=False, **settings.SCREEN)
-player = Player()
-
-while not libtcod.console_is_window_closed():
-    key = libtcod.console_check_for_keypress()
-    exited = key.vk == libtcod.KEY_ESCAPE
-    if exited:
-        break
-    libtcod.console_set_default_foreground(0, libtcod.white)
-    player.render(graphics)
-    graphics.flush()
+if __name__ == '__main__':
+    main()
--- roguelike/settings.py
+++ roguelike/settings.py
@@ -5,4 +5,3 @@ LIMIT_FPS = 20
 STATIC_DIR = 'static'
 FONT_DIR = os.path.join(STATIC_DIR, 'font')
 VARS_FILE = 'vars.yml'
-os.path.dirname(__file__)
